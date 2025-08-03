cca51b0ae97fb75e79f4e81d383a1abbf4965a2a
amit <amitnarwal115@gmail.com>
2025-08-03 12:27:00 +0530

    Adding all files

--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -11,7 +11,7 @@ type ScreenContentProps = {
 export const ScreenContent = ({ title, path, children }: ScreenContentProps) => {
   return (
     <View className={styles.container}>
-      <Text className={styles.title}>{title}</Text>
+      <Text className='text-red-500 bg-blue-200 w-full'>{title}</Text>
       <View className={styles.separator} />
       <EditScreenInfo path={path} />
       {children}
